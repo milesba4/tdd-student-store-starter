63099cf0d084195c60c9c693083de64a1736777f
milesba4 <milesbaffour@gmail.com>
2022-06-16 17:45:38 -0400

    importing useState and linking '/' route to Logo component

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,17 +1,20 @@
 import * as React from "react"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
 
-const products = useState([])
+
+
 export default function App() {
+
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/"> <Home/> <Navbar/> <Sidebar/> </Route>
+          <Route path="/"> <Home/> <Navbar/> <Sidebar/> <Logo/></Route>
           <Route path="/products/:productId"> <ProductDetail/> <Navbar/> <Sidebar/> </Route>
           <Route path="*"> <NotFound/><Navbar/> <Sidebar/> </Route>
 
