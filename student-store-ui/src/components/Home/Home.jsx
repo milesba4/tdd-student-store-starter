428a71bb98c38c9e1ba90d11b42b3fd37b0d9dc1
milesba4 <milesbaffour@gmail.com>
2022-06-16 18:01:01 -0400

    Rendering Hero and ProductGrid component inside Home component

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -4,6 +4,8 @@ import "./Home.css"
 export default function Home({products,handleAddItemToCart,handleRemoveItemToCart}) {
   return (
     <div className="home">
+      <Hero/>
+      <ProductGrid/>
       <p>Home</p>
     </div>
   )
