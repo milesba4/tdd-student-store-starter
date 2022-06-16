647ef0f2fee3e396e6547f21964d844667c76e81
milesba4 <milesbaffour@gmail.com>
2022-06-16 17:20:53 -0400

    Creating route paths in Core App component,  creating "products" state variable

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,14 +1,24 @@
 import * as React from "react"
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
 
+const products = useState([])
 export default function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Routes>
+          <Route path="/"> <Home/> <Navbar/> <Sidebar/> </Route>
+          <Route path="/products/:productId"> <ProductDetail/> <Navbar/> <Sidebar/> </Route>
+          <Route path="*"> <NotFound/><Navbar/> <Sidebar/> </Route>
+
+
+
+
+        </Routes>
         <main>
           {/* YOUR CODE HERE! */}
           <Navbar />
