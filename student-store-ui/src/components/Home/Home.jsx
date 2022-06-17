87ee8fe06fab26ea5b77d0ad0a79cdeb669f5fb3
milesba4 <milesbaffour@gmail.com>
2022-06-17 13:51:36 -0400

    passing UserInput and setUserInput props into Search component

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -2,11 +2,13 @@ import * as React from "react"
 import "./Home.css"
 import Hero from "../Hero/Hero"
 import ProductGrid from "../ProductGrid/ProductGrid"
-export default function Home({products,handleAddItemToCart,handleRemoveItemToCart}) {
+import Search from "../Search/Search"
+export default function Home({products,handleAddItemToCart,handleRemoveItemToCart, UserInput, setUserInput}) {
   return (
     <div className="home">
-       <Hero/> 
+      <Hero/> 
       <h2>Best Selling Products</h2>
+      <Search UserInput={UserInput} setUserInput={setUserInput}/>
       <ProductGrid products={products}/> 
      
     </div>
