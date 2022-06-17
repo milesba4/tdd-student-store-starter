043a74da053ef75fd462ad2c7096e003f89795b3
milesba4 <milesbaffour@gmail.com>
2022-06-17 14:07:35 -0400

    Displaying price and title inside product card

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -4,6 +4,8 @@ export default function ProductCard({element}) {
     return (
       <div>
         <img src = {element.image} width="200" height="200"/>
+        <p className="name"> {element.name}</p>
+        <p className="price"> ${element.price}</p>
       </div>
     )
   }
