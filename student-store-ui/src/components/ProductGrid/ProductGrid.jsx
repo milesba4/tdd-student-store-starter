ecfab1c9a37c93de78e829cd01690268dd2453a9
milesba4 <milesbaffour@gmail.com>
2022-06-16 22:18:38 -0400

    rendering ProductCard component in ProductGrid component

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -1,7 +1,12 @@
+import * as React from "react"
+import ProductCard from "../ProductCard/ProductCard"
 export default function ProductGrid({}) {
     return (
         <div className="product-grid">
-
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
         </div>
 
 )
