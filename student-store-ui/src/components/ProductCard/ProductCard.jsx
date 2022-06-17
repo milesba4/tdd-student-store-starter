291e36aea5813d3b02e536652257f51b72261e4e
milesba4 <milesbaffour@gmail.com>
2022-06-17 13:01:57 -0400

    rendering a div tag with class "product-card" for each product

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,14 @@
-export default function ProductCard({}) {
+import * as React from "react"
+import "./ProductCard.css"
+export default function ProductCard({products}) {
     return (
-      <p>Product Card</p>
+      <div>
+        {products.map((element) =>{
+        return(
+        <div className="product-card"> 
+        </div>
+        )})}
+      </div>
     )
   }
   
