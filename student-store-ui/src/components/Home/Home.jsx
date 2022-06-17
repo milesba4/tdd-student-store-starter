d81a978bdc8f5e2839a7071deac195c186b38106
milesba4 <milesbaffour@gmail.com>
2022-06-16 23:27:24 -0400

    importing ProductGrid component and Hero component into Home.jsx

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import * as React from "react"
 import "./Home.css"
-
+import Hero from "../Hero/Hero"
+import ProductGrid from "../ProductGrid/ProductGrid"
 export default function Home({products,handleAddItemToCart,handleRemoveItemToCart}) {
   return (
     <div className="home">
-      <Hero/>
-      <ProductGrid/>
+       <Hero/> 
+      <ProductGrid/> 
       <p>Home</p>
     </div>
   )
