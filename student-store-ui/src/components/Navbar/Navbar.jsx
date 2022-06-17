e0f86bb518e9ad28275d3975213d351b534b1db8
milesba4 <milesbaffour@gmail.com>
2022-06-16 23:27:38 -0400

    Adding links to navbar

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -2,11 +2,26 @@ import * as React from "react"
 import "./Navbar.css"
 import Logo from "../Logo/Logo"
 export default function Navbar() {
+
   return (
     <nav className="navbar">
-      <p>Navbar</p>
-      
+      <ul class="nav-links">
       <Logo/>
+      <li>
+        <a href=""> Home</a>
+      </li>
+      <li>
+        <a href=""> About Us</a>
+      </li>
+      <li>
+        <a href=""> Contact Us</a>
+      </li>
+      <li>
+        <a href=""> Buy Now</a>
+      </li>
+      </ul>
+
+      
     </nav>
   )
 }
