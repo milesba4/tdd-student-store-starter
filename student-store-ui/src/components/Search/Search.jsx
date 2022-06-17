be2b968d6cf431997bf6b46da95e44ce0221e0df
milesba4 <milesbaffour@gmail.com>
2022-06-17 13:44:52 -0400

    creating an onChange event handler in search form

--- student-store-ui/src/components/Search/Search.jsx
+++ student-store-ui/src/components/Search/Search.jsx
@@ -3,7 +3,7 @@ export default function Search({UserInput,setUserInput}) {
     return (
         <form className="Search-form">
             <label>
-                <input type="text" placeholder="Search" name="Search" value={UserInput} />
+                <input type="text" placeholder="Search" name="Search" value={UserInput} onChange={evt=>setUserInput(evt.target.value)}/>
             </label>
             <input type="submit" value="Submit" />
         </form>
