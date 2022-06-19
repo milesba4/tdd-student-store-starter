e5d6cdad308972dc90554d83538460ba833346de
milesba4 <milesbaffour@gmail.com>
2022-06-19 19:27:54 -0400

    writing place holder in hero component

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -1,7 +1,10 @@
+import "./Hero.css"
+
 export default function Hero({}) {
     return (
     <div className="hero">
     <h1 className="intro">Welcome! </h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates culpa reprehenderit ad necessitatibus ipsam, ea tempora fugiat saepe et, numquam porro ratione cumque sit veniam temporibus magnam dolores, deleniti adipisci! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa exercitationem maxime maiores atque, ullam ratione! Impedit numquam eaque eveniet ipsum ipsa, nulla rerum nemo natus quasi consectetur at eos repellendus? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis assumenda facilis illo. Accusantium doloremque id consequuntur ut nulla, debitis aliquid animi autem nesciunt nobis sequi odio ab quis laborum similique.</p>
     </div>
         
 )
