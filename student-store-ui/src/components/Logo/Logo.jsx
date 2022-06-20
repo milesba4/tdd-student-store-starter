bfc7d521a00887b46b9c08d9009deea506badf92
milesba4 <milesbaffour@gmail.com>
2022-06-20 00:16:00 -0400

    creating function for search queries and category selections

--- student-store-ui/src/components/Logo/Logo.jsx
+++ student-store-ui/src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo(){
 return(
 <div className="logo">
-    (Logo)
+
 </div>
 
 )
--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -3,6 +3,26 @@ import "./ProductGrid.css"
 import ProductCard from "../ProductCard/ProductCard"
 
 export default function ProductGrid({products}) {
+
+export default function ProductGrid({products, selectCategory,userInput}) {
+
+    console.log("userInput=",userInput)
+
+    function getSearchProducts(){
+        if(userInput=="" && selectCategory=="All Categories"){  // if search query is nothing return all products
+        return products
+        }
+        else if (selectCategory=="All Categories" && userInput != ""){ 
+          return  products.filter((e)=>e.name.toLowerCase().includes(userInput))
+        }
+        else{   // otherwise return an array of products in which the search query value is in the product's name
+        console.log("userInput2=", userInput)
+        console.log("filter=",products.filter((e)=>e.name.includes(userInput.toLowerCase())))
+        return products.filter((e)=>e.category == selectCategory.toLowerCase() && e.name.toLowerCase().includes(userInput))// return an arr of categories where all categories are return
+        }
+    }
+
+
     return (
     <div className="product-grid">
      {products.map((element) =>{
