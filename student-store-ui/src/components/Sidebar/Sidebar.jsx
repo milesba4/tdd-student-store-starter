53d7b1d85f36f0c0c298013a7632d579c4820087
milesba4 <milesbaffour@gmail.com>
2022-06-23 16:22:04 -0400

    creating dynamic classname for section element

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,7 @@ import ShoppingCart from "../ShoppingCart/ShoppingCart"
 import CheckoutForm from "../CheckoutForm/CheckoutForm"
 export default function Sidebar({isOpen, ShoppingCart,products, checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm,handleOnToggle}) {
   return (
+    <section className={isOpen ? "sidebar" : "sidebar closed"}>
 
   )
 }
