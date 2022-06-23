7f6084507039da65c5eedf2a03c6420c019f0691
milesba4 <milesbaffour@gmail.com>
2022-06-23 19:21:16 -0400

    rendering product number in h1 tag inside productView component

--- student-store-ui/src/components/ProductView/ProductView.jsx
+++ student-store-ui/src/components/ProductView/ProductView.jsx
@@ -1,14 +1,11 @@
 import * as React from "react"
 import ProductCard from "../ProductCard/ProductCard"
-export default function ProductView({}) {
+export default function ProductView({products,productId,quantity,handleAddItemToCart,handleRemoveItemToCart}) {
     return (
         <div className = "product-view">
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+        <h1 className="product-id">Product #{productId}</h1>
+        <ProductCard products={products}/>
         </div>
-
 )
 
 
