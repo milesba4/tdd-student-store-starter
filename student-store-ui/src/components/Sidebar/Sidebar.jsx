788e078693c9b065fd70befd48bdd627093b92d7
milesba4 <milesbaffour@gmail.com>
2022-06-23 16:21:37 -0400

    Update Sidebar.jsx

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -4,24 +4,6 @@ import ShoppingCart from "../ShoppingCart/ShoppingCart"
 import CheckoutForm from "../CheckoutForm/CheckoutForm"
 export default function Sidebar({isOpen, ShoppingCart,products, checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm,handleOnToggle}) {
   return (
-    <section className="sidebar">
-      <button className="toggle-button" onClick={()=>handleOnToggle()}> toggle</button>
-      {/* <p>Sidebar</p>
-      
-      <ul>
-        <li>
-          <a href=""> Home</a>
-        </li>
-        <li>
-          <a href=""> About Us</a>
-        </li>
-        <li>
-          <a href=""> Contact Us</a>
-        </li>
-        <li>
-          <a href=""> Buy Now</a>
-        </li>
-      </ul> */}
 
       {<ShoppingCart/>}
       {<CheckoutForm/>}
