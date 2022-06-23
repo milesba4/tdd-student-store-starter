4b02c28e982210a2766987b8b10f50b69a3a8120
milesba4 <milesbaffour@gmail.com>
2022-06-23 16:25:43 -0400

    linking ProductCard component to dynamic route containing productId

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,11 @@
 import * as React from "react"
 import "./ProductCard.css"
-export default function ProductCard({element}) {
+import {Link } from "react-router-dom";
+
+export default function ProductCard({element,productId}) {
     return (
       <div>
-        <img src = {element.image} width="200" height="200"/>
+        <Link to={`/products/${productId}`}><img src = {element.image} width="200" height="200"/></Link>
         <p className="name"> {element.name}</p>
         <p className="price"> ${element.price}</p>
       </div>
