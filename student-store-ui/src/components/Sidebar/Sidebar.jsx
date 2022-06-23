7a6c5f9dcc70f4cab55dcad081c5de691a74c485
milesba4 <milesbaffour@gmail.com>
2022-06-23 16:22:11 -0400

    adding toggle button

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,7 @@ import CheckoutForm from "../CheckoutForm/CheckoutForm"
 export default function Sidebar({isOpen, ShoppingCart,products, checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm,handleOnToggle}) {
   return (
     <section className={isOpen ? "sidebar" : "sidebar closed"}>
+        <button className="toggle-button" onClick={()=>handleOnToggle()}> 
 
   )
 }
