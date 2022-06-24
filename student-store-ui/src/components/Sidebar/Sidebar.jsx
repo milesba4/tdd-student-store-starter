40670c5c93c1cd9887484775b09146b7248d54f2
milesba4 <milesbaffour@gmail.com>
2022-06-24 15:45:04 -0400

    conditionally rendering ShoppingCart and CheckourForm component inside Sidebar component

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -9,9 +9,10 @@ export default function Sidebar({isOpen,handleAddItemToCart, shoppingCart,produc
         <button className={isOpen ? "toggle-button button open" : "toggle-button"} onClick={()=>handleOnToggle()}> 
         <i className="material-icons md-48">arrow_forward</i>
         </button> 
-         {<><ShoppingCart products={products} handleAddItemToCart ={handleAddItemToCart} shoppingCart={shoppingCart}/>
-         <CheckoutForm/></>
-         } 
+        {isOpen &&
+        <><ShoppingCart isOpen={isOpen} products={products} shoppingCart={shoppingCart} />
+          <CheckoutForm isOpen={isOpen} shoppingCart={shoppingCart} checkoutForm={checkoutForm} handleOnCheckoutFormChange={handleOnCheckoutFormChange}
+            handleOnSubmitCheckoutFormChange={handleOnSubmitCheckoutForm} error={error} /> </>} 
 
   </section>
   )
