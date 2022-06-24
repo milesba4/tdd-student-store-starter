b823b5e1cdd9b5647a09ceee446a42a4a13446d3
milesba4 <milesbaffour@gmail.com>
2022-06-24 16:02:02 -0400

    Update ProductCard.jsx

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -2,16 +2,16 @@ import * as React from "react";
 import "./ProductCard.css";
 import { Link } from "react-router-dom";
 
-export default function ProductCard({ element, productId }) {
+export default function ProductCard({handleAddItemToCart, element, productId }) {
   return (
     <div>
-      <Link to={`/products/${productId}`}>
+      <Link to={"/products/" + productId}>
         <img src={element.image} width="200" height="200" />
       </Link>
       <p className="name"> {element.name}</p>
       <p className="price"> ${element.price}</p>
       <div class="buttons">
-        <button class="add">
+        <button class="add" onClick={()=>handleAddItemToCart()}>
           <i class="material-icons md32">add</i>
         </button>
         <button class="remove">
