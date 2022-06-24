d3299b7938371f90c0fa9fe3d03bb85c7170bd4d
milesba4 <milesbaffour@gmail.com>
2022-06-24 12:10:52 -0400

    Adding an add and remove button to product card component

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,14 +1,23 @@
-import * as React from "react"
-import "./ProductCard.css"
-import {Link } from "react-router-dom";
+import * as React from "react";
+import "./ProductCard.css";
+import { Link } from "react-router-dom";
 
-export default function ProductCard({element,productId}) {
-    return (
-      <div>
-        <Link to={`/products/${productId}`}><img src = {element.image} width="200" height="200"/></Link>
-        <p className="name"> {element.name}</p>
-        <p className="price"> ${element.price}</p>
+export default function ProductCard({ element, productId }) {
+  return (
+    <div>
+      <Link to={`/products/${productId}`}>
+        <img src={element.image} width="200" height="200" />
+      </Link>
+      <p className="name"> {element.name}</p>
+      <p className="price"> ${element.price}</p>
+      <div class="buttons">
+        <button class="add">
+          <i class="material-icons">add</i>
+        </button>
+        <button class="remove">
+          <i class="material-icons">remove</i>
+        </button>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
