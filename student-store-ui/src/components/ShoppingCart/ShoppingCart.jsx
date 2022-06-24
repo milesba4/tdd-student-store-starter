31fc372b34d8e7df67ec4076dcece9ccb67b09f9
milesba4 <milesbaffour@gmail.com>
2022-06-23 22:16:31 -0400

    displaying product name and quantity for each element in shoppingCart object

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,7 +1,22 @@
 import * as React from "react"
-export default function ShoppingCart({isOpen,products,shoppingCart}) {
+export default function ShoppingCart({shoppingCart,products}) {
 return(
-<div className = "shopping-cart"> 
-</div>
+ <div className = "shopping-cart"> 
+ 
+    {shoppingCart.map((item) =>{   // iterating through shoppingCart array and displaying the itemId of each object 
+        console.log("item s",item)
+        let itemProduct = products.find(e => e.id === item.itemId) // store product object if matches id
+        console.log("name=", itemProduct)
+        return(
+            <div className= "cart-product-info">
+                <div className="cart-product-name">
+                {itemProduct}   
+                </div>
+                <div className="cart-product-quantity">
+                {item.quantity}   
+                </div>
+            </div>
+        )})}
+</div> 
 )
  }
