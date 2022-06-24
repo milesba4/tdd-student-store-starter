d84de10ee3565bdc1f997b191aea84e67ccaf0c4
milesba4 <milesbaffour@gmail.com>
2022-06-24 17:14:11 -0400

    Checkout Form template

--- student-store-ui/src/components/checkoutForm/checkoutForm.jsx
+++ student-store-ui/src/components/checkoutForm/checkoutForm.jsx
@@ -1,6 +1,55 @@
 export default function CheckoutForm() {
-    return (
-        <p>Checkout</p>
-  
-    )
-  }
+  return (
+    <div class="wrapper">
+      <div class="checkout-form">
+        <h3 class="">
+          Payment Info{" "}
+          <span class="button">
+            <i class="material-icons md-48">monetization_on</i>
+          </span>
+        </h3>
+        <div class="input-field">
+          <label class="label">Name</label>
+          <div class="control ">
+            <input
+              name="name"
+              class="checkout-form-input"
+              type="text"
+              placeholder="Student Name"
+              value=""
+            ></input>
+          </div>
+        </div>
+        <div class="input-field">
+          <label class="label">Email</label>
+          <div class="control">
+            <input
+              name="email"
+              class="checkout-form-input"
+              type="email"
+              placeholder="student@codepath.org"
+              value=""
+            ></input>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <label class="checkbox">
+              <input name="termsAndConditions" type="checkbox"></input>
+              <span class="label">
+                I agree to the{" "}
+                <a href="#terms-and-conditions">terms and conditions</a>
+              </span>
+            </label>
+          </div>
+        </div>
+        <p class="is-danger"></p>
+        <div class="field">
+          <div class="control">
+            <button class="button checkout-button">Checkout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
