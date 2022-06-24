21d785bcc1a7811dc0066867674bb1b953234755
milesba4 <milesbaffour@gmail.com>
2022-06-24 12:08:39 -0400

    Changing size of logo

--- student-store-ui/src/components/Logo/Logo.jsx
+++ student-store-ui/src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo(){
 return(
 <div className="logo">
-<img src="https://codepath-student-store-demo.surge.sh/assets/codepath.f1b3e41a.svg"></img>
+<img src="https://codepath-student-store-demo.surge.sh/assets/codepath.f1b3e41a.svg" width="48" height="48"></img>
 </div>
 
 )
