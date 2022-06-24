9a31225688ca9d47a6d9976e621af87c1b8b4f11
milesba4 <milesbaffour@gmail.com>
2022-06-24 17:14:34 -0400

    Adding class to intro

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero({}) {
             <div class="intro">
                 <h1>Welcome!</h1>
                 <h1>Find Your Merch!</h1>
-                <p>We have all kinds of goodies. Click on any of the items to start filling up your shopping cart. Checkout whenever you're ready.</p>
+                <p class = "intro">We have all kinds of goodies. Click on any of the items to start filling up your shopping cart. Checkout whenever you're ready.</p>
             </div>
             <div class="media">
                 <img src="https://codepath-student-store-demo.surge.sh/assets/student_store_icon.18e5d61a.svg" alt="hero" class="hero-img" width="200" height="200"></img>
