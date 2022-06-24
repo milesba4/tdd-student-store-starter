aad0ab5ff5c2535b891ac84279f244d939e6f1f3
milesba4 <milesbaffour@gmail.com>
2022-06-23 21:36:56 -0400

    creating a GET request handler to "/" endpoint

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,11 @@
-// YOUR CODE HERE
+
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+
+module.exports=app // exporting app as default
+app.use(morgan("tiny"))
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.status(200).send({products})
+  })
