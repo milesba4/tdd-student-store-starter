04bef226f90e23492587bafd40fb87e53bab87ea
milesba4 <milesbaffour@gmail.com>
2022-06-24 12:08:09 -0400

    Changing structure of hero component

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -2,13 +2,19 @@ import "./Hero.css"
 
 export default function Hero({}) {
     return (
-    
-    <div className="hero">
-    <h1 className="intro">Welcome! </h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates culpa reprehenderit ad necessitatibus ipsam, ea tempora fugiat saepe et, numquam porro ratione cumque sit veniam temporibus magnam dolores, deleniti adipisci! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa exercitationem maxime maiores atque, ullam ratione! Impedit numquam eaque eveniet ipsum ipsa, nulla rerum nemo natus quasi consectetur at eos repellendus? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis assumenda facilis illo. Accusantium doloremque id consequuntur ut nulla, debitis aliquid animi autem nesciunt nobis sequi odio ab quis laborum similique.</p>
-    <img src="https://codepath-student-store-demo.surge.sh/assets/student_store_icon.18e5d61a.svg" alt="hero" class="hero-img"></img>
+     <div class="hero">
+        <div class="content">
+            <div class="intro">
+                <h1>Welcome!</h1>
+                <h1>Find Your Merch!</h1>
+                <p>We have all kinds of goodies. Click on any of the items to start filling up your shopping cart. Checkout whenever you're ready.</p>
+            </div>
+            <div class="media">
+                <img src="https://codepath-student-store-demo.surge.sh/assets/student_store_icon.18e5d61a.svg" alt="hero" class="hero-img" width="200" height="200"></img>
+            </div>
+        </div>
     </div>
-        
+    
 )
 
 
