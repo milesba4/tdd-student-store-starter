05ea963bdeb5208256ff3b8b50ebd491244ec547
milesba4 <milesbaffour@gmail.com>
2022-06-24 12:03:43 -0400

    Update app.js

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,8 +1,8 @@
-import App from "../student-store-ui/src/components/App/App"
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
-
+const router = require("./routes/store");
+app.use("/store", router);
 module.exports=app // exporting app as default
 app.use(morgan("tiny"))
 app.use(express.json())
