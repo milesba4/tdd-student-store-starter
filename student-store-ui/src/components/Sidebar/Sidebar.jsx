78a1cd305e0342f6ad788ff1eb46b7c7b787ead8
milesba4 <milesbaffour@gmail.com>
2022-06-23 22:22:18 -0400

    passing props into sidebar component

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,8 @@ import * as React from "react"
 import "./Sidebar.css"
 import ShoppingCart from "../ShoppingCart/ShoppingCart"
 import CheckoutForm from "../CheckoutForm/CheckoutForm"
-export default function Sidebar({isOpen, ShoppingCart,products, checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm,handleOnToggle}) {
+export default function Sidebar({isOpen, shoppingCart,products, checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm,handleOnToggle, error}) {
+
   return (
     <section className={isOpen ? "sidebar" : "sidebar closed"}>
         <button className="toggle-button" onClick={()=>handleOnToggle()}> 
