ca7cbac7c69c1042dfe59227cd703fd91b7d0e34
milesba4 <milesbaffour@gmail.com>
2022-06-24 12:10:02 -0400

    Passing handleAddItem to cart into ShoppingCart component

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -2,14 +2,14 @@ import * as React from "react"
 import "./Sidebar.css"
 import ShoppingCart from "../ShoppingCart/ShoppingCart"
 import CheckoutForm from "../CheckoutForm/CheckoutForm"
-export default function Sidebar({isOpen, shoppingCart,products, checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm,handleOnToggle, error}) {
+export default function Sidebar({isOpen,handleAddItemToCart, shoppingCart,products, checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm,handleOnToggle, error}) {
 
   return (
     <section className={isOpen ? "sidebar" : "sidebar closed"}>
         <button className={isOpen ? "toggle-button button open" : "toggle-button"} onClick={()=>handleOnToggle()}> 
         <i className="material-icons md-48">arrow_forward</i>
         </button> 
-         {<><ShoppingCart products={products} shoppingCart={shoppingCart}/>
+         {<><ShoppingCart products={products} handleAddItemToCart ={handleAddItemToCart} shoppingCart={shoppingCart}/>
          <CheckoutForm/></>
          } 
 
