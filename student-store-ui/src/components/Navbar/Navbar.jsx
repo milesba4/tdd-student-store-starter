b88546f2867d89477cf9c8d71ab287d260d1ba12
milesba4 <milesbaffour@gmail.com>
2022-06-23 22:19:19 -0400

    importing link in navbar

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import * as React from "react"
 import "./Navbar.css"
 import Logo from "../Logo/Logo"
+import { Link } from "react-router-dom";
 export default function Navbar() {
 
   return (
     <nav className="navbar">
       <ul className="nav-links">
+        
         <li>
           <Logo/>
         </li>
       <li>
-        <a href=""> Home</a>
+      <a href=""> Home</a>
       </li>
       <li>
         <a href=""> About Us</a>
