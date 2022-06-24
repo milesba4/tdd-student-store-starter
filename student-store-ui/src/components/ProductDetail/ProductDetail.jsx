4369c5a66f0ae8a85185d4b4c0b1897d7f97583b
milesba4 <milesbaffour@gmail.com>
2022-06-24 15:43:09 -0400

    extracting productId from URL

--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -1,12 +1,27 @@
 import * as React from "react"
-import ProductCard from "../ProductCard/ProductCard"
 import ProductView from "../ProductView/ProductView"
-export default function ProductDetail({products, handleOnToggle}) {
-return(
-<div> 
-    <ProductView products={products}/>
-    Hello
-</div>
+import NotFound from "../NotFound/NotFound"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+export default function ProductDetail({error,setError,shoppingCart,products, handleOnToggle,handleAddItemToCart,handleRemoveItemToCart,isFetching,setIsFetching}) {
+const [product, setProduct] = React.useState({})
 
+//extracting productId from URL
+const { productId } = useParams()
+
+React.useEffect(async() => {
+    try{
+        const response = await axios.get("http://localhost:3001/store/" + productId)
+        console.log("fetch response = ",response) // checking response
+        if(response){
+        setProduct(response.data.product)
+        console.log("rdp=",response.data.product)
+        setIsFetching(false)}
+    }
+    catch (error){
+        setIsFetching(false)
+        setError(error)
+    return <NotFound />
+    }}, []);
 )
  }
