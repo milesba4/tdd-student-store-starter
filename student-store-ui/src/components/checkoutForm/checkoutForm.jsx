7a522ca9368baee426bdd476a91d75d4f22a5fd1
milesba4 <milesbaffour@gmail.com>
2022-06-25 12:52:09 -0400

    passing in props to CheckoutForm

--- student-store-ui/src/components/checkoutForm/checkoutForm.jsx
+++ student-store-ui/src/components/checkoutForm/checkoutForm.jsx
@@ -1,4 +1,5 @@
 export default function CheckoutForm() {
+export default function CheckoutForm(isOpen,shoppingCart,checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm) {
   return (
         </div>
     </div>
