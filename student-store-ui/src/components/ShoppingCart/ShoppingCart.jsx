6e175abba2b9aa6347000d3c489003b0bc060efa
milesba4 <milesbaffour@gmail.com>
2022-06-25 10:35:48 -0400

    Making dynamic shopping cart component display

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,25 +1,31 @@
 import * as React from "react"
 export default function ShoppingCart({shoppingCart,products}) {
-    console.log("Sh=",shoppingCart)
-return(
- <div className = "shopping-cart"> 
-    {shoppingCart.map((item) =>{   // iterating through shoppingCart array and displaying the itemId of each object 
-        console.log("item s",item)
-        let itemProduct= products.find(e => e.id === item.itemId) 
-        console.log("name=", itemProduct)
-        let quantity = item.quantity
-        let price = itemProduct.price
-        let totalPrice = quantity * price
-        return(
-            <div className= "cart-product-info">
-                <span className="cart-product-name">Item: {itemProduct.name}</span>
-                 <br></br>
-                <span className="cart-product-quantity">Quantity:{quantity}</span> 
-                <br></br>
-                <span className="cart-product-total-price">Price:  {totalPrice}</span> 
-            </div>
+    return(
 
-        )})}
-</div> 
-)
+    <div className = "shopping-cart"> 
+    {shoppingCart.length!=0?(<getShoppingCartItems shoppingCart={shoppingCart} products={products}/>):<h1>None</h1>}
+    </div> 
+    )
+ }
+export function getShoppingCartItems({shoppingCart,products}){
+    console.log("works")
+    return(
+        shoppingCart.map((item) =>{   // iterating through shoppingCart array and displaying the itemId of each object 
+            console.log("item s",item)
+            let itemProduct= products.find(e => e.id === item.itemId) 
+            console.log("name=", itemProduct)
+            let quantity = item.quantity
+            let price = itemProduct.price
+            let totalPrice = quantity * price
+            
+            return(
+                <div className= "cart-product-info">
+                    <span className="cart-product-name">Item: {itemProduct.name}</span>
+                     <br></br>
+                    <span className="cart-product-quantity">Quantity:{quantity}</span> 
+                    <br></br>
+                    <span className="cart-product-total-price">Price:  {totalPrice}</span> 
+                </div>
+            )})
+    )
  }
