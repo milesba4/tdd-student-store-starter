56938785396b452023c5b3512de4198e71d7329e
milesba4 <milesbaffour@gmail.com>
2022-06-24 22:25:16 -0400

    calculating and displaying the price, item , and quantity of items in shopping cart

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -4,14 +4,19 @@ export default function ShoppingCart({shoppingCart,products}) {
 return(
  <div className = "shopping-cart"> 
     {shoppingCart.map((item) =>{   // iterating through shoppingCart array and displaying the itemId of each object 
-        let quantity = item.quantity
         console.log("item s",item)
         let itemProduct= products.find(e => e.id === item.itemId) 
         console.log("name=", itemProduct)
+        let quantity = item.quantity
+        let price = itemProduct.price
+        let totalPrice = quantity * price
         return(
             <div className= "cart-product-info">
-                <span className="cart-product-name">{itemProduct.name} </span> 
-                <span className="cart-product-quantity">{quantity}</span> 
+                <span className="cart-product-name">Item: {itemProduct.name}</span>
+                 <br></br>
+                <span className="cart-product-quantity">Quantity:{quantity}</span> 
+                <br></br>
+                <span className="cart-product-total-price">Price:  {totalPrice}</span> 
             </div>
         )})}
 </div> 
