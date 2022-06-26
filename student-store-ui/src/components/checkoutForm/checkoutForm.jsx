d09daf41310df86764d937ffa6d167aa082aa319
milesba4 <milesbaffour@gmail.com>
2022-06-25 23:17:23 -0400

    creating name form

--- student-store-ui/src/components/checkoutForm/checkoutForm.jsx
+++ student-store-ui/src/components/checkoutForm/checkoutForm.jsx
@@ -3,6 +3,7 @@ export default function CheckoutForm(isOpen,shoppingCart,checkoutForm,handleOnCh
     <div classNme="wrapper">
       <div className="checkout-form">
         <input className ="checkout-form-input" type = "email" name = "email" placeholder = "student@codepath.org" value = "checkoutForm.email" onChange={handleOnCheckoutFormChange()}/>
+        <input className = "checkout-form-input" type = "text" name = "name" placeholder = "Student Name" value = "checkoutForm.name" onChange = {handleOnCheckoutFormChange()}/>
         </div>
 
     </div>
