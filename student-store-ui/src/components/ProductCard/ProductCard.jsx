d32743eb3d16cf920417fa60863f1723d8046941
milesba4 <milesbaffour@gmail.com>
2022-06-25 23:20:16 -0400

    Creating component for quantity display and commenting it out

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import "./ProductCard.css";
 import { Link } from "react-router-dom";
 
-export default function ProductCard({handleAddItemToCart,handleRemoveItemToCart, element, productId }) {
+
+export default function ProductCard({shoppingCart,handleAddItemToCart,handleRemoveItemToCart, element, productId }) {
+  console.log("el=", element)
   return (
     <div>
       <Link to={"/products/" + productId}>
@@ -18,17 +20,25 @@ export default function ProductCard({handleAddItemToCart,handleRemoveItemToCart,
           <i class="material-icons md32">remove</i>
         </button>
       </div>
+      <div className = "quantity-display">
+      {/* <QuantityDisplay productId={productId} shoppingCart={shoppingCart} element = {element} />     */}
+      </div>
     </div>
   );
 }
 
-export function QuantityDisplay({}){
-return(
-<div className = "quantity-display"></div>
+// export function QuantityDisplay({shoppingCart, productId}){
+//   console.log("prodid",productId)
+//   let index = shoppingCart.findIndex(item=> item.itemId == productId) // finding index of object that contains matching id
+//   console.log("ind=", index)
+// return(
+// <div className = "quantity">
+// {shoppingCart[index].quantity}
+// </div>
 
 
-)
+// )
 
 
 
-}
+//}
