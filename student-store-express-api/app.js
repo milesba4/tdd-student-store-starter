941a54b37a9ab8112983a8f747e585f33a2f17ff
milesba4 <milesbaffour@gmail.com>
2022-06-26 15:43:36 -0400

    setting up error handling in app.js file

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,11 +1,31 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
-const router = require("./routes/store");
+const router = require("./routes/store")
+const{NotFoundError}= require("./utils/errors")
 app.use("/store", router);
-module.exports=app // exporting app as default
+
 app.use(morgan("tiny"))
 app.use(express.json())
+
+app.use((req,res,next)=>{
+  return next(new NotFoundError())
+})
+
+app.use((error,req,res,next)=>{
+const status = error.status || 500
+const message = error.message
+
+return res.status(status).json({
+  error:{message,status}
+})
+
+})
+
 app.get('/', (req, res) => {
     res.status(200).send({ping:"pong"})
   })
+
+
+
+  module.exports=app // exporting app as default
