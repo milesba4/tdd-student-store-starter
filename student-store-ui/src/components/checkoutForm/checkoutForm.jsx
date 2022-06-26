fdfee2385b8cd7c2c3aef693e94c2031696096e3
milesba4 <milesbaffour@gmail.com>
2022-06-25 23:17:39 -0400

    creating checkout button

--- student-store-ui/src/components/checkoutForm/checkoutForm.jsx
+++ student-store-ui/src/components/checkoutForm/checkoutForm.jsx
@@ -4,6 +4,7 @@ export default function CheckoutForm(isOpen,shoppingCart,checkoutForm,handleOnCh
       <div className="checkout-form">
         <input className ="checkout-form-input" type = "email" name = "email" placeholder = "student@codepath.org" value = "checkoutForm.email" onChange={handleOnCheckoutFormChange()}/>
         <input className = "checkout-form-input" type = "text" name = "name" placeholder = "Student Name" value = "checkoutForm.name" onChange = {handleOnCheckoutFormChange()}/>
+        <button className = "checkout-button" onClick = {()=>{handleOnSubmitCheckoutForm}}>Checkout</button>
         </div>
 
     </div>
