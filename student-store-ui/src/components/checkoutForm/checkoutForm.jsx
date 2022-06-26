235774d9b48b41e8d7cf7fbbc3440f8437a8b9c5
milesba4 <milesbaffour@gmail.com>
2022-06-25 23:17:07 -0400

    creating form for email

--- student-store-ui/src/components/checkoutForm/checkoutForm.jsx
+++ student-store-ui/src/components/checkoutForm/checkoutForm.jsx
@@ -1,7 +1,10 @@
-export default function CheckoutForm() {
 export default function CheckoutForm(isOpen,shoppingCart,checkoutForm,handleOnCheckoutFormChange,handleOnSubmitCheckoutForm) {
   return (
+    <div classNme="wrapper">
+      <div className="checkout-form">
+        <input className ="checkout-form-input" type = "email" name = "email" placeholder = "student@codepath.org" value = "checkoutForm.email" onChange={handleOnCheckoutFormChange()}/>
         </div>
+
     </div>
   );
 }
