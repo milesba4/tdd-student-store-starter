fcd0389e4b713a2dc39197b2a2323e4987ae5d4a
milesba4 <milesbaffour@gmail.com>
2022-06-26 22:28:15 -0400

    Update store.js

--- student-store-express-api/routes/store.js
+++ student-store-express-api/routes/store.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const Store = ("../models/store")
-const {BadRequestError, NotFoundError} = require("../utils/errors");
+const Store = require("../models/store")
+const {BadRequestError,NotFoundError} = require("../utils/errors")
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
